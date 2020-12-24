b1db3c9c5571df80632bd4d2c0ea55ba3c3a4ce2
H. Almohri <almohri@gmail.com>
2020-12-24 10:36:22 +0300

    Create simple_io.py

---
+++ simple_io.py
@@ -0,0 +1,16 @@
+#This is a very simple I/O program for CS126 students. 
+
+#Take two numbers from the user with the input function.
+#The input function only returns data objects of type string.
+x = input()
+y = input()
+
+#Convert a string of numbers to float.
+x = float(x)  
+y = float(y) 
+
+#Compute x divided by z. 
+z = x/y 
+
+#Print the results on the screen.
+print(z) 
