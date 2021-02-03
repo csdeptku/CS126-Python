25059f45c4f01479b19ad2c03947111a2f1f2dc7
Hussain Almohri <almohri@cs.ku.edu.kw>
2021-02-03 13:35:37 +0300

    removing the unwanted spaces from the string

--- code-samples/check_numeric.py
+++ code-samples/check_numeric.py
@@ -2,7 +2,7 @@
 Check if a string has either an integer or a float in it. 
 '''
 s = input('Enter a value: ') 
-if s.replace('.','').replace(' ','').isnumeric() and s.count('.') < 2: 
+if s.replace('.','').rstrip().lstrip().isnumeric() and s.count('.') < 2: 
    print(s,'is numeric')
 else:
    print(s, 'is not numeric') 
