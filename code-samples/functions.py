11a6ee89be9e211d25df8b13cc936f2905dc2470
Hussain Almohri <almohri@cs.ku.edu.kw>
2021-02-08 19:12:39 +0300

    added an index function

--- code-samples/functions.py
+++ code-samples/functions.py
@@ -26,15 +26,34 @@ def length(L):
       i+=1
    return i
 
+def maxall(L):
+   m = L[0]
+   for x in L:
+      if m < x:
+         m = x
+   return m 
+
+def index(L,e):
+   i = 0 
+   for x in L:
+      if x == e: 
+         return i
+      i+=1
+   return i+1 
+
 L=input().split()
 L=list(map(eval,L))
 
 s = summation(L)
 n = length(L)
+m = maxall(L)
 print('Sum:',s)
 print('Length:',n)
+print('Maximum:',m)
 if n>=3:
    print('Max of first three numbers:', maximum(L[0],L[1],L[2]))
 
 print('Squared:')
 print(list(map(square,L)))
+print('Index of',1,'is',index(L,1))
+
