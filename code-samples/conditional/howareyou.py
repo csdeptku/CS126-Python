589e8bdc3eca66f8816f61728cf517f9ba584b39
Hussain Almohri <almohri@cs.ku.edu.kw>
2021-08-23 08:46:40 +0300

    modified msg

--- code-samples/conditional/howareyou.py
+++ code-samples/conditional/howareyou.py
@@ -5,6 +5,6 @@ A simple program to print an answer based on the user's answer.
 ans = input('How are you?') 
 
 if ans.find('good') > -1 or ans.find('fine') > -1 or ans.find('ok') > -1: 
-  print('Cool!')
+  print('Great!')
 else:
-  print('I wish that you feel better tomorrow.')
+  print('Feel better tomorrow.')
