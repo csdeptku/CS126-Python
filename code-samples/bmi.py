7825dc85e2efb0d676cb49a9e343b8c946fec6ec
Hussain Almohri <almohri@cs.ku.edu.kw>
2021-08-23 08:35:57 +0300

    updated bmi

--- code-samples/bmi.py
+++ code-samples/bmi.py
@@ -1,4 +1,5 @@
-#This program computes the body mass index of a person, given the person's height and weight. 
+
+#compute a person's body mass index
 
 height = float(input("Enter your height: "))
 weight = float(input("Enter your weight: "))
@@ -6,3 +7,13 @@ weight = float(input("Enter your weight: "))
 bmi = weight/((height*.01)**2)
 bmi = round(bmi, 2)
 print("BMI: ", bmi)
+
+if bmi <= 18.5: 
+   print("Underweight")
+elif bmi <25:
+   print("Normal weight")
+elif bmi < 30:
+   print("Overweight")
+else: 
+   print("Obesity")
+
