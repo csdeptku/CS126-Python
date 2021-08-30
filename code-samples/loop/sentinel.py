c01640aa6089c48fd34c338040dae9d79f3f3296
Hussain Almohri <almohri@cs.ku.edu.kw>
2021-08-30 11:31:04 +0300

    modified example

--- code-samples/loop/sentinel.py
+++ code-samples/loop/sentinel.py
@@ -3,14 +3,14 @@
 Write a program to receive numbers from the user.
 When the user finishes entering the numbers, print
 the sum of the numbers. Assume the user is finished when
-the value is -1. 
+the value is END. 
 '''
 
 s = 0 
 while True:
-    value = int(input('Enter a value: '))
-    if value == -1:
-        break
-    s += value
+   value = input()
+   if value == 'END':
+      break
+   s+= eval(value)
 
-print('The sum is',s)
+print(s)
