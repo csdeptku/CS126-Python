121ecf006b676688117eb3f847a0b33e1090775f
Hussain Almohri <almohri@cs.ku.edu.kw>
2021-09-08 13:16:31 +0300

    fixed secondmin

---
+++ code-samples/advanced/secondmin.py
@@ -0,0 +1,11 @@
+from math import inf
+def secondmax(L):
+   m1,m2=inf,inf
+   for x in L:
+      tmp = None
+      if x <= m1:
+         tmp = m1 
+         m1  = x
+      if (tmp is not None) and tmp<m2:
+         m2 = tmp 
+   return m2
