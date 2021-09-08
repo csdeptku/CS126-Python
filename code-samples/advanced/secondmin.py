e04e43ba41def1dc19a3faf8f8090a6ec45146be
Hussain Almohri <almohri@cs.ku.edu.kw>
2021-09-08 13:22:49 +0300

    fixed secondmin

--- code-samples/advanced/secondmin.py
+++ code-samples/advanced/secondmin.py
@@ -2,10 +2,9 @@ from math import inf
 def secondmin(L):
    m1,m2=inf,inf
    for x in L:
-      tmp = None
       if x <= m1:
-         tmp = m1 
+         m2 = m1 
          m1  = x
-      if (tmp is not None) and tmp<m2:
-         m2 = tmp 
+      elif x < m2: 
+         m2 = x 
    return m2
